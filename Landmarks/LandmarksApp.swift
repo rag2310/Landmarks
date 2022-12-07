fa67672b4af00e0d624a948f1eb5ee4d36dc33dd
Rodolfo Gutierrez <rgutierrez@trackchain.io>
2022-12-07 13:12:19 -0600

    [FEATURE] creating-and-combining-views

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Rodolfo Gutierrez on 7/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
